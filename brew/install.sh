0563b92a7a6210613a6383f2d05904fa9b6a2fde
James Boehmer <james.boehmer@gmail.com>
2022-02-06 21:18:57 -0500

    Cleaned up brew package installation

--- brew/install.sh
+++ brew/install.sh
@@ -5,23 +5,15 @@
 which brew &>/dev/null
 if [[ $? -ne 0 ]]
 then
-	if [[ "$(uname -p)" == "arm" ]]
-	then
-		sudo mkdir -p /opt/homebrew/Caskroom /opt/homebrew/Cellar;
-		sudo chown -fR "${USER}" /opt/homebrew;
-		pushd /opt;
-		curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew;
-		HOMEBREW_DIR="/opt/homebrew";
-		CASKROOM_DIR="${HOMEBREW_DIR}/Caskroom";
-		CELLAR_DIR="${HOMEBREW_DIR}/Cellar";
-		export PATH="/opt/homebrew/bin:$PATH";
-	else
-		HOMEBREW_DIR="/usr/local/Homebrew";
-		CASKROOM_DIR="/usr/local/Caskroom";
-		CELLAR_DIR="/usr/local/Cellar";
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
-	fi
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+HOMEBREW_DIR="$(brew config | grep HOMEBREW_PREFIX | awk '{print $2}')";
+CASKROOM_DIR="${HOMEBREW_DIR}/Caskroom";
+CELLAR_DIR="${HOMEBREW_DIR}/Cellar";
+export PATH="${HOMEBREW_DIR}/bin:$PATH";
+
+brew install git;
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -32,16 +24,8 @@ do
 	[[ -e "${tapdir}" ]] && echo "Already tapped: ${tap}" || brew tap "${tap}";
 done < ${BASEDIR}/taps.txt
 
-while read cask
-do
-	basecask="$(basename ${cask})";
-    [[ -e "${CASKROOM_DIR}/${basecask}" ]] || compgen -G "${HOME}/Library/Caches/Homebrew/Cask/${basecask}--*" >/dev/null && echo "Already installed: ${basecask}" || brew install --cask "${cask}";
-done < ${BASEDIR}/casks.txt
-
-while read package
-do
-    [[ -e "/usr/local/opt/${package}" || -e "${CELLAR_DIR}/${package}" || -e "${HOMEBREW_DIR}/opt/${package}" ]] && echo "Already installed: ${package}" || brew install "${package}";
-done < ${BASEDIR}/packages.txt
+xargs brew install --cask < ${BASEDIR}/casks.txt
+xargs brew install < ${BASEDIR}/packages.txt
 
 # only install mas on high sierra
 defaults read loginwindow SystemVersionStampAsString | grep "10.13" &>/dev/null && brew install mas;
@@ -50,10 +34,10 @@ defaults read loginwindow SystemVersionStampAsString | grep "10.13" &>/dev/null 
 # brew cask install sonos;
 
 # Sublime convenience script
-ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "/usr/local/bin/subl";
+# ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "/usr/local/bin/subl";
 
 # Little Snitch
-if [[ ! -e "/Applications/Little Snitch Configuration.app" ]]
+if [[ ! -e "/Applications/Little Snitch.app" ]]
 then
 	LITTLESNITCH_VERSION="$(/bin/ls -1 ${CASKROOM_DIR}/little-snitch/ | sort | tail -1)";
 	LITTLESNITCH_INSTALLER="${CASKROOM_DIR}/little-snitch/${LITTLESNITCH_VERSION}/LittleSnitch-${LITTLESNITCH_VERSION}.dmg";
@@ -62,6 +46,7 @@ fi
 
 # Configure homebrew permissions to allow multiple users on MAC OSX.
 # Any user from the admin group will be able to manage the homebrew and cask installation on the machine.
+
 for brewdir in "${CELLAR_DIR}" "${CASKROOM_DIR}" "/Library/Caches/Homebrew"
 do
 	[[ -e "${brewdir}" ]] || sudo mkdir -p "${brewdir}" &>/dev/null;
