b62325e8c5f2d14c1707511000b5e119e0f23bdf
James Boehmer <james.boehmer@gmail.com>
2023-01-03 19:11:51 -0500

    Added nodenv on linux env

--- apt/install.sh
+++ apt/install.sh
@@ -26,16 +26,25 @@ fi
 
 if [[ ! -e ~/.pyenv ]]
 then
-	mkdir -p ~/.private
+	mkdir -p ~/.local
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv;
 	git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
-	echo 'export PYENV_ROOT="$HOME/.pyenv"' > ~/.private/pyenvrc
-	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.private/pyenvrc
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' > ~/.local/pyenvrc
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.local/pyenvrc
 fi
 
 if [[ ! -e ~/.tfenv ]]
 then
-	mkdir -p ~/.private
+	mkdir -p ~/.local
 	git clone https://github.com/tfutils/tfenv.git ~/.tfenv;
-	echo 'export PATH="$HOME/.tfenv/bin:$PATH"' > ~/.private/tfenvrc
+	echo 'export PATH="$HOME/.tfenv/bin:$PATH"' > ~/.local/tfenvrc
+fi
+
+if [[ ! -e ~/.nodenv ]]
+then
+	mkdir -p ~/.local
+	git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+	mkdir -p ~/.nodenv/plugins
+	git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+	echo 'export PATH="$HOME/.nodenv/bin:$PATH"' > ~/.local/nodenvrc 
 fi
