b0244b0c60f711521961db148678d304df18f681
James Boehmer <james.boehmer@gmail.com>
2024-01-09 20:27:15 -0500

    Refactored iterm profile installation

--- macinstall/macinstall.sh
+++ macinstall/macinstall.sh
@@ -27,28 +27,50 @@ mkdir -p ~/.local;
 ln -sf "$(pwd)/mac.gitconfig" "${HOME}/.local/mac.gitconfig";
 git config --file ~/.local/.gitconfig --replace-all include.path '~/.local/mac.gitconfig' '~/.local/mac.gitconfig'
 
-echo "Setting up iTerm2 defaults..."
+DYNAMIC_PROFILES_DIR="${HOME}/Library/Application Support/iTerm2/DynamicProfiles";
+mkdir -p "${DYNAMIC_PROFILES_DIR}";
 
-mkdir -p ~/.iterm
-cat com.googlecode.iterm2.plist.template | envsubst > ~/.iterm/com.googlecode.iterm2.plist
+ln -sf "$(pwd)/iterm2-profiles.json" "${DYNAMIC_PROFILES_DIR}/iterm2-profiles.json";
 
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string ${HOME}/.iterm
-defaults write com.googlecode.iterm2.plist SUEnableAutomaticChecks -bool false
-defaults write com.apple.gamed Disabled -bool true
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true # disable creation of .DS_Store files
-defaults write com.googlecode.iterm2.plist BootstrapDaemon -bool false #makes sudo thumbprint ID work
 defaults read com.googlecode.iterm2.plist >/dev/null
 defaults read -app iTerm >/dev/null
 
-# Enable keyboard to be used to navigate dialogs.
+echo "Setting up iTerm2 profiles...";
+OLD_DEFAULT_PROFILE_GUID="$(defaults read com.googlecode.iterm2.plist "Default Bookmark Guid" 2>/dev/null)";
+DEFAULT_PROFILE_GUID="$(cat iterm2-profiles.json | jq -r '.Profiles[] | select(.Name=="Default") | .Guid' | head -1)";
+if [[ "${DEFAULT_PROFILE_GUID}" == "" ]]; then
+    echo "No Default profile found in iterm2-profiles.json.  You'll need to switch manually in iTerm2.";
+elif [[ "${OLD_DEFAULT_PROFILE_GUID}" != "${DEFAULT_PROFILE_GUID}" ]]; then
+    defaults write com.googlecode.iterm2.plist "Default Bookmark Guid" -string "${DEFAULT_PROFILE_GUID}";
+    echo "Default profile is set, but you'll need to restart iTerm2.  Do not open preferences until after you restart";
+fi
+
+echo "Enabling iTerm2 automatic update checks...";
+defaults write com.googlecode.iterm2.plist SUEnableAutomaticChecks -bool true
+
+echo "Enabling iTerm2 sudo thumbprint...";
+defaults write com.googlecode.iterm2.plist BootstrapDaemon -bool false #makes sudo thumbprint ID work
+# defaults read com.googlecode.iterm2.plist >/dev/null
+# defaults read -app iTerm >/dev/null
+
+
+echo "Disabling Game Center...";
+defaults write com.apple.gamed Disabled -bool true
+
+echo "Disabling .DS_Store files...";
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+
 # This probably won't take effect until after you open the sysprefs->keyboard->shortcuts window or restart.
 # See https://stackoverflow.com/a/54192723
+echo "Enabling keyboard to be used to navigate dialogs (you need to restart for this to take effect)...";
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
 
 # disable the macos tips thing
+echo "Disabling apple tips...";
 sudo launchctl disable system/com.apple.tipsd
 
+echo "Adding osxkeychain to docker credsStore...";
 DOCKERCONFIG=~/.docker/config.json;
 echo "Setting ${DOCKERCONFIG} credsStore to osxkeychain";
 mkdir -p ~/.docker;
