748b19f27e3c817db461b1a8da08c6e2d53df6ba
James Boehmer <james.boehmer@gmail.com>
2024-10-22 09:59:41 -0400

    Removed direnv use_nodenv.sh now that it's merged.  Added export_function.sh

---
+++ xdgconfig/direnv/lib/export_function.sh
@@ -0,0 +1,15 @@
+export_function() {
+  local alias_dir=$PWD/.direnv/aliases
+  mkdir -p "$alias_dir"
+  PATH_add "$alias_dir"
+  for name in ${@}; do
+    local name=$1
+    local target="$alias_dir/$name"
+    if declare -f "$name" >/dev/null; then
+      echo "#!/usr/bin/env ${SHELL}" > "$target"
+      declare -f "$name" >> "$target" 2>/dev/null
+      echo "$name" >> "$target"
+      chmod +x "$target"
+    fi
+  done
+}
