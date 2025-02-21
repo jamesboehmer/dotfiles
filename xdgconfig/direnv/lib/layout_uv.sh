dd85a33a1be731ba9e2288ac5696a5710a85bd95
James Boehmer <james.boehmer@gmail.com>
2025-02-21 02:24:22 +0000

    Added direnv layout_uv.sh

---
+++ xdgconfig/direnv/lib/layout_uv.sh
@@ -0,0 +1,28 @@
+# Usage: layout uv <python version number>
+#
+# Example:
+#
+#    layout uv cpython-3.11.11-linux-aarch64-gnu
+#    layout uv cpython-3.12
+#    layout uv v3.10.16
+#    layout uv 3.10
+#
+# Uses uv create and load a virtual environment.
+# "$direnv_layout_dir/uv/$python_version".
+#
+layout_uv() {
+  if ! has uv; then
+      log_error "uv is not installed"
+      return 1
+  fi
+  local uv_version="${1}";
+  local uv_layout_path="$(direnv_layout_dir)/uv/${uv_version}";
+  if ! uv -q --no-config venv -p ${uv_version} "${uv_layout_path}"; then
+    log_error "uv venv failed";
+    return 1
+  fi
+  export VIRTUAL_ENV="$(readlink -f ${uv_layout_path})";
+  export UV_PROJECT_ENVIRONMENT="${VIRTUAL_ENV}";
+  export UV_PYTHON="${uv_version}";
+  PATH_add "${VIRTUAL_ENV}/bin";
+}
