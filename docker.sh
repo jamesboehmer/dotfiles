a980b4e74355007b0516ae21bfa77936ad657727
James Boehmer <james.boehmer@gmail.com>
2025-03-27 20:53:09 -0400

    Enable cliPluginsExtraDirs and linux config to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-[[ "$(uname -s)" == "Darwin" ]] || { echo "Not OSX.  Skipping mac docker config." && exit 0; }
-
 echo "Configuring docker...";
 
 DOCKERCONFIG="${HOME}/.docker/config.json";
-echo "Setting ${DOCKERCONFIG} credsStore to osxkeychain";
+
+BREWPREFIX="$(type brew &>/dev/null && brew --prefix || echo "")";
+PLUGINDIRS=("${BREWPREFIX}/lib/docker/cli-plugins" "/usr/libexec/docker/cli-plugins");
+
 mkdir -p $(dirname ${DOCKERCONFIG});
-if [[ -e ${DOCKERCONFIG} ]]
-then
-    # sponge is included in moreutils
-    jq '.credsStore="osxkeychain"' ${DOCKERCONFIG} | sponge ${DOCKERCONFIG}
-else
-    echo '{}' | jq '.credsStore="osxkeychain"' | sponge ${DOCKERCONFIG}
-fi
+
+[[ ! -e ${DOCKERCONFIG} ]] && echo '{}' > "${DOCKERCONFIG}";
+
+[[ "$(uname -s)" == "Darwin" ]] && echo "Setting credsStore to osxkeychain" && jq '.credsStore="osxkeychain"' "${DOCKERCONFIG}" | sponge "${DOCKERCONFIG}";
+
+for PLUGINDIR in ${PLUGINDIRS[@]}; do
+    [[ -e "${PLUGINDIR}" ]] && echo "Adding ${PLUGINDIR} to cliPluginsExtraDirs" && cat "${DOCKERCONFIG}" | jq --arg PLUGINDIR "${PLUGINDIR}" '.cliPluginsExtraDirs=((.cliPluginsExtraDirs // [])  + [$PLUGINDIR] | sort | unique)' | sponge "${DOCKERCONFIG}";
+done
