b14302065801078e869e9da91442ad71a869483a
James Boehmer <750457+jamesboehmer@users.noreply.github.com>
2025-10-08 19:52:14 +0000

    Removed sponge from docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+THIS="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/$(basename ${BASH_SOURCE[0]})";
+THISDIR="$(dirname "${THIS}")";
+. "${THISDIR}/functions.sh";
+
 echo "Configuring docker...";
 
 DOCKERCONFIG="${HOME}/.docker/config.json";
@@ -11,8 +15,9 @@ mkdir -p $(dirname ${DOCKERCONFIG});
 
 [[ ! -e ${DOCKERCONFIG} ]] && echo '{}' > "${DOCKERCONFIG}";
 
-[[ "$(uname -s)" == "Darwin" ]] && echo "Setting credsStore to osxkeychain" && jq '.credsStore="osxkeychain"' "${DOCKERCONFIG}" | sponge "${DOCKERCONFIG}";
+DOCKERCONFIGTMP="$(mktemp)";
+[[ "${KERNEL}" == "darwin" ]] && echo "Setting credsStore to osxkeychain" && cat "${DOCKERCONFIG}" | jq '.credsStore="osxkeychain"' > "${DOCKERCONFIGTMP}" && mv "${DOCKERCONFIGTMP}" "${DOCKERCONFIG}";
 
 for PLUGINDIR in ${PLUGINDIRS[@]}; do
-    [[ -e "${PLUGINDIR}" ]] && echo "Adding ${PLUGINDIR} to cliPluginsExtraDirs" && cat "${DOCKERCONFIG}" | jq --arg PLUGINDIR "${PLUGINDIR}" '.cliPluginsExtraDirs=((.cliPluginsExtraDirs // [])  + [$PLUGINDIR] | sort | unique)' | sponge "${DOCKERCONFIG}";
+    [[ -e "${PLUGINDIR}" ]] && echo "Adding ${PLUGINDIR} to cliPluginsExtraDirs" && cat "${DOCKERCONFIG}" | jq --arg PLUGINDIR "${PLUGINDIR}" '.cliPluginsExtraDirs=((.cliPluginsExtraDirs // [])  + [$PLUGINDIR] | sort | unique)' > "${DOCKERCONFIGTMP}"; mv "${DOCKERCONFIGTMP}" "${DOCKERCONFIG}";
 done
