edc9d49a78a376e50d9d26889bc3d2a203ef4c9f
James Boehmer <750457+jamesboehmer@users.noreply.github.com>
2026-02-14 17:45:39 +0000

    Watch aws-vault env file

--- direnv/lib/use_aws-vault.sh
+++ direnv/lib/use_aws-vault.sh
@@ -3,8 +3,9 @@ use_aws-vault() {
 #
 # Invokes aws-vault to create  AWS environment variables for the specified profile.
 # The environment variables are stored in a file under the direnv layout directory.
-    which aws-vault &>/dev/null;
-    [[ $? -ne 0 ]] && log_error "aws-vault is not installed" && return 1;
+    # which aws-vault &>/dev/null;
+    # [[ $? -ne 0 ]] && 
+    has aws-vault || { log_error "aws-vault is not installed" && return 1; }
     [[ $# -lt 1 ]] && log_error "Usage: aws-vault <profile>" && return 1;
     profile=$1;
     DIRENV_LAYOUT_DIR="$(direnv_layout_dir)";
@@ -12,6 +13,7 @@ use_aws-vault() {
     ENVFILE="${DIRENV_LAYOUT_DIR}/aws-vault/${profile}.env";
     touch "${ENVFILE}";
     chmod 600 "${ENVFILE}";
+    # eval "$(aws-vault export --format=export-env "${profile}")";
     aws-vault export --format=export-env "${profile}" >"${ENVFILE}";
-    . "${ENVFILE}";
+    source_env_if_exists "${ENVFILE}";
 }
